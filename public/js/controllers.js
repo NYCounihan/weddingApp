64ba44cebc6c4ac1fed69afac19187fa110d3825
Julian Counihan <juliancounihan@gmail.com>
2015-04-13 23:43:07 -0400

    formatting

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,7 +6,7 @@ var weddingControllers = angular.module('weddingControllers', []);
 
 weddingControllers.controller('ScheduleCtrl',  function($scope) {
     $scope.MainTitle = "We’re Getting Married";
-    $scope.SubTitle = "SATURDAY, 25th JULY 2015";
+    $scope.SubTitle = "SATURDAY, 25<sup>th</sup> JULY 2015";
 }); 
 
 weddingControllers.controller('RSVPCtrl', function($scope) {
